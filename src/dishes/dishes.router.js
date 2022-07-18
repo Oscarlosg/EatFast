2636e59312d8150dbbd66472e71c3d7545ab3008
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2022-07-18 10:50:50 -0400

    init commit

---
+++ src/dishes/dishes.router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+// TODO: Implement the /dishes routes needed to make the tests pass
+
+router.route("/").get(controller.list).all(methodNotAllowed);
+
+router.route("/:dishId").all(methodNotAllowed)
+
+module.exports = router;
