5cf07d44f98c3b6bbde337cae0c75a2cc4c288fe
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2023-11-07 19:00:07 -0500

    updated the look of the site. Gave it a logo for better brand identity.

--- client/src/layout/Header.js
+++ client/src/layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import headerImage from "./header.jpg";
+import logo from "./logo.png"
 
 const style = {
   background: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url(${headerImage})`,
@@ -11,9 +12,9 @@ function Header() {
   return (
     <div className="jumbotron jumbotron-fluid text-white mb-0" style={style}>
       <div className="container">
-        <h1 className="display-1">GrubDash</h1>
+        <img src={logo} alt="" className="ml-4 pb-3 pt-3" />
         <p className="lead">
-          The best restaurants near you deliver with <em>GrubDash</em>!
+          Eat Good, <em>Eat Fast</em>!
         </p>
       </div>
     </div>
